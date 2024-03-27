f03392975d662165ce0b6225b8c5ccd18de8962a
Nathan Drake <nathandrakedrums@gmail.com>
2024-03-27 14:41:51 -0500

    updated starting place

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,9 @@
-function App() {
-  return <h1 className="text-purple-500">test</h1>;
-}
+const App = (): React.JSX.Element => {
+  return (
+    <main className="flex h-svh flex-col items-center gap-12 bg-gray-100 p-8 font-mono text-black dark:bg-gray-950 dark:text-white">
+      <h1 className="text-5xl font-semibold">TypeScript Demo</h1>
+    </main>
+  );
+};
 
 export default App;
