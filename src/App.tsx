c1a24da7d1ee3ff61ec7af5c05b409beecccba9d
Nathan Drake <ndrake@paylocity.com>
2024-03-28 12:09:51 -0500

    after session

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,22 @@
-const App = (): React.JSX.Element => {
+import type { ReactNode } from "react";
+
+import Button from "./components/button";
+import Layout from "./components/layout";
+
+const App = (): ReactNode => {
   return (
     <main className="flex h-svh flex-col items-center gap-12 bg-gray-100 p-8 font-mono text-black dark:bg-gray-950 dark:text-white">
       <h1 className="text-5xl font-semibold">TypeScript Demo</h1>
+
+      <Layout className="flex gap-4">
+        <Button id="text-button" type="button">
+          Click Me
+        </Button>
+
+        <Button id="text-button" type="button">
+          Click Me
+        </Button>
+      </Layout>
     </main>
   );
 };
